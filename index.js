1bd3d2730686439e6b87f434ab24c7088d24ce66
Apethesis <apethesis@gmail.com>
2022-12-12 19:21:22 +0100

    fixed some things

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('express')();
-const path = require('fs');
+const fs = require('fs');
 const port = 9551;
 
 app.get('/download', (req, res) => {
@@ -14,7 +14,7 @@ app.get('/download', (req, res) => {
 
 app.get('/store/:item', (req, res) => {
   const fpath = __dirname + `/files/store/${req.params.item}.cfg`
-  if (path.existsSync(fpath)) {
+  if (fs.existsSync(fpath)) {
     res.sendFile(fpath);
   } else {
     res.status(404).send(`notFound`).end();
